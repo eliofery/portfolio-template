a880aef448280946ceafeb7856d28a85d48ee562
Sergey Erofeev <erofsey@yandex.kz>
2023-08-29 14:22:47 +0600

    fix read-progress and main-blog

--- src/js/components/read-progress.js
+++ src/js/components/read-progress.js
@@ -6,9 +6,10 @@ import { getElementsByData } from '@/_utils'
 
 const readProgress = () => {
   const { 'read-progress': progress } = getElementsByData('el')
-  const maxScreenHeight = document.body.scrollHeight - window.screen.height
 
   const setProgress = () => {
+    const maxScreenHeight = document.body.scrollHeight - window.screen.height
+
     progress.hidden = window.scrollY <= 0
     progress.value = Math.floor((window.scrollY * 100) / maxScreenHeight)
   }
